7a5498501eb5cee51f5fed3fda34a7ee4fbc273c
James Sgarella <Jimbo8702@gmail.com>
2021-10-25 18:35:38 -0400

    js outlined

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//Dependeces
+//data
+//Functions
+function renderForcast() {}
+
+function renderCurrentDay() {}
+
+function apiCurrentWeather() {}
+
+function apiForcast() {}
+
+//user interactions
+//initilization
