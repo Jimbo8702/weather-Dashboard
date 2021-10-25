8711560c7d3d2c0ffaf3f09fc4d11c03b62dc5d9
James Sgarella <Jimbo8702@gmail.com>
2021-10-25 18:36:11 -0400

    forgot to save

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,14 @@
 //data
 //Functions
 function renderForcast() {}
-
+// displays the forcast on the screen
 function renderCurrentDay() {}
-
+//displays the current day on screen
 function apiCurrentWeather() {}
-
+//gets todays weather
 function apiForcast() {}
+//gets the forcast
 
 //user interactions
+
 //initilization
